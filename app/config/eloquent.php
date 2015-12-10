fe1cd807a8d32ead83b9203b58ee71abcc25e504
Gauthier Delamarre <gauthier@objective-php.org>
2015-12-10 12:07:15 +0100

    Update Eloquent config, add favicon

--- app/config/eloquent.php
+++ app/config/eloquent.php
@@ -4,12 +4,11 @@
      * @var $config ObjectivePHP\Config\Config
      */
 
-    $dbConfig = $config->eloquent;
-
-    $dbConfig->driver   = 'mysql';
-    $dbConfig->host     = '127.0.0.1';
-    $dbConfig->port     = 3306;
-    $dbConfig->username = 'demo';
-    $dbConfig->password = '';
-    $dbConfig->database = 'employees';
-
+    $config->eloquent->add(
+        [
+            'driver'   => 'mysql',
+            'host'     => '127.0.0.1',
+            'username' => 'demo',
+            'password' => '',
+            'database' => 'employees'
+        ]);
